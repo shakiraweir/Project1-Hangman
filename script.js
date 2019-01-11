5e86d2aa646de79ec3682d39e77dbf28c9e9aa4c
Shakira Weir <shakira.weir87@gmail.com>
2019-01-10 21:10:18 -0500

    hide ability to input a letter until a word has been entered

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ window.onload = function displayHanger() {
     ctx.lineTo(220,60);
     ctx.stroke();
 }
+
+hideLetterInputElements(); //Hide ability to input a letter until a word has been entered
+
+function hideLetterInputElements() {
+    document.getElementById('inputLetterSection').style.display = 'none';
+}
+function hideWordInputElements() {
+    document.getElementById('inputWordSection').style.display = 'none';
+}
+
+function showLetterInputElements() {
+    document.getElementById('inputLetterSection').style.display = 'block';
+}
