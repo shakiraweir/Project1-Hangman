9df25a6fed37dc668b9b3a0b3a0dc87ef5f94d5b
Shakira Weir <shakira.weir87@gmail.com>
2019-01-10 22:25:45 -0500

    created arrayOfBodyParts set to list each function that displays a hangman body part

--- script.js
+++ script.js
@@ -21,8 +21,46 @@ hideLetterInputElements(); //Hide ability to input a letter until a word has bee
  var currentWord = []; //currentWord will be set to the most recent word typed in the word text box
  var currentLetter = ""; //currentLetter will be set to the most recent letter typed in the letter text box
  var totalLettersFound = 0; //totaLettersFound will be set to total letters guessed correctly for the currentWord
+ var arrayOfBodyParts = [ //arrayOfBodyParts will be set to a list of each hangman part to be displayed when wrong letter is entered
+    function displayRope() { //7 parts of the array will equal the total amount of tries a user has to enter an incorrect letter before game is lost
+        ctx.stroke();
+        ctx.moveTo(220,60);
+        ctx.lineTo(220,110);
+        ctx.stroke();
+    },
+    function displayHead() {
+        ctx.beginPath();
+        ctx.arc(220, 140, 30, 0, 2 * Math.PI);
+        ctx.stroke();
+    },
+    function displayBody() {
+        ctx.moveTo(220,170);
+        ctx.lineTo(220,250);
+        ctx.stroke();
+    },  
+    function displayLeftLeg() {
+        ctx.moveTo(220,250);
+        ctx.lineTo(180,300);
+        ctx.stroke();
+    },            
+    function displayRightLeg()  {
+        ctx.moveTo(220,250);
+        ctx.lineTo(260,300);
+        ctx.stroke();
+    },
+     function displayleftArm() {
+        ctx.moveTo(220,210);
+        ctx.lineTo(180,195);
+        ctx.stroke();
+     },                
+    function displayRightArm() {
+        ctx.moveTo(220,210);
+        ctx.lineTo(260,195);
+        ctx.stroke();
+    }
+]
 
- //Hide and reveal sections of the game
+//Hide and reveal sections of the game
 function hideLetterInputElements() {
     document.getElementById('inputLetterSection').style.display = 'none';
 }
@@ -33,3 +71,4 @@ function hideWordInputElements() {
 function showLetterInputElements() {
     document.getElementById('inputLetterSection').style.display = 'block';
 }
+
