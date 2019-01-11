4297e3ecf0a0c6d38458277487421df08047b937
Shakira Weir <shakira.weir87@gmail.com>
2019-01-11 00:16:23 -0500

    created function to process currentLetter

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ hideLetterInputElements(); //hide ability to input a letter until a word has bee
  var currentWord = []; //currentWord will be set to the most recent word typed in the word text box
  var currentLetter = ""; //currentLetter will be set to the most recent letter typed in the letter text box
  var totalLettersFound = 0; //totaLettersFound will be set to total letters guessed correctly for the currentWord
+ var currentHangmanPartDisplayed = 0; //currentHangmanPartDisplayed will increment by 1 as body part is revealed for each wrong word
  var arrayOfBodyParts = [ //arrayOfBodyParts is set to a list of each hangman part to be displayed when wrong letter is entered
     function displayRope() { //7 parts of the array will equal the total amount of tries a user has to enter an incorrect letter before game is lost
         ctx.stroke();
@@ -63,7 +64,7 @@ hideLetterInputElements(); //hide ability to input a letter until a word has bee
 //store and process each letter input and word input when button is clicked
 const wordInput = document.getElementById("inputWordTxt");
 const letterInput = document.getElementById("inputLetterTxt")
-const messageBox = document.getElementById("display");
+const displayBox = document.getElementById("display");
 
 //series of steps that happen when word is entered
 function storeWord(evt) { // onclick() called in html tag
@@ -81,8 +82,7 @@ const letters = document.getElementById('letters');
 
 function displayEmptyWordTemplate() {
     for (i = 0; i < currentWord[0].length; i++) {
-        var word = currentWord[0].charAt(i);
-        var letter = document.createElement('li');
+        let letter = document.createElement('li');
         letter.innerHTML = '_';
         letters.appendChild(letter);
     }   
@@ -100,10 +100,32 @@ function storeLetter(evt) { //onclick() called in html tag
 
 function storeLetterInLetterBox() {
     // show letter output
-    messageBox.innerHTML = "";
-    messageBox.innerHTML += currentLetter + " ";
+    let display = document.createElement('div');
+    displayBox.innerHTML += currentLetter + " ";
+    displayBox.appendChild(display)
 }
 
+
+function processLetter(currentLetter) {
+    // If the letter is in the word, display it in the appropriate position in the word template
+    // Loop through word finding all instances of guessed letter, store the indicies in an array
+    for (i = 0; i < currentWord[0].length; i++) {
+        let word = currentWord[0].charAt(i);
+        let temp = currentWord.toString().split("");
+        let letterMatch = function(element) {
+            return element === currentLetter
+            }
+        if(currentLetter === word) {
+            letters.children[i].innerHTML = currentLetter;
+        } 
+        if (temp.some(letterMatch) !== true) {
+            arrayOfBodyParts[currentHangmanPartDisplayed++](); 
+            break;
+        }  
+    }   
+}              
+
+    
 //hide and reveal sections of the game
 function hideLetterInputElements() {
     document.getElementById('inputLetterSection').style.display = 'none';
@@ -116,3 +138,7 @@ function showLetterInputElements() {
     document.getElementById('inputLetterSection').style.display = 'block';
 }
 
+//major issues
+//issues- getting words with two of the same letter to display
+//getting hangman part to display
+//figuring out how to dynamically create an element
